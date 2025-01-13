3ea63da095e5b187360045c1da3e326c0bf2be3e
이승환 <seaung13@gmail.com>
2025-01-14 03:21:31 +0900

    test: 트랜잭션을 적용할 경우, 정상 계좌 이체 및 이체 중 예외 발생 테스트 추가

---
+++ src/main/java/com/chain/jdbc/service/MemberServiceV2.java
@@ -0,0 +1,61 @@
+package com.chain.jdbc.service;
+
+import com.chain.jdbc.domain.Member;
+import com.chain.jdbc.repository.MemberRepositoryV2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - 파리미터로 커넥션 받기, 커넥션 풀을 고려한 종료
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV2 {
+
+    private final DataSource dataSource;
+    private final MemberRepositoryV2 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        Connection conn = dataSource.getConnection();
+
+        try {
+            conn.setAutoCommit(false);
+            bizLogic(fromId, toId, money, conn);
+            conn.commit();
+        } catch (Exception e) {
+            conn.rollback();
+            throw new IllegalStateException(e);
+        } finally {
+            release(conn);
+        }
+
+    }
+
+    private void bizLogic(String fromId, String toId, int money, Connection conn) throws SQLException {
+        Member fromMember = memberRepository.findById(conn, fromId);
+        Member toMember = memberRepository.findById(conn, toId);
+
+        memberRepository.update(conn, fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(conn, toId, toMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+
+    private void release(Connection conn) {
+        try {
+            conn.setAutoCommit(true);
+            conn.close();
+        } catch (Exception e) {
+            log.info("error", e);
+        }
+    }
+}
---
+++ src/test/java/com/chain/jdbc/service/MemberServiceV2Test.java
@@ -0,0 +1,82 @@
+package com.chain.jdbc.service;
+
+import com.chain.jdbc.domain.Member;
+import com.chain.jdbc.repository.MemberRepositoryV1;
+import com.chain.jdbc.repository.MemberRepositoryV2;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.SQLException;
+
+import static com.chain.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class MemberServiceV2Test {
+
+    public static final String MEMBER_A = "memberA";
+    public static final String MEMBER_B = "memberB";
+    public static final String MEMBER_EX = "ex";
+
+    private MemberRepositoryV2 memberRepository;
+    private MemberServiceV2 memberService;
+
+    @BeforeEach
+    void beforeEach() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        memberRepository = new MemberRepositoryV2(dataSource);
+        memberService = new MemberServiceV2(dataSource, memberRepository);
+    }
+
+    @AfterEach
+    void afterEach() throws SQLException {
+        memberRepository.delete(MEMBER_A);
+        memberRepository.delete(MEMBER_B);
+        memberRepository.delete(MEMBER_EX);
+    }
+
+    @DisplayName("트랜잭션을 적용할 경우, 정상 이체 상황에서 계좌의 금액이 성공적으로 변경되면 커밋된다")
+    @Test
+    void accountTransfer() throws SQLException {
+        // given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberB = new Member(MEMBER_B, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+
+        // when
+        memberService.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000);
+
+        // then
+        Member findMemberA = memberRepository.findById(memberA.getMemberId());
+        Member findMemberB = memberRepository.findById(memberB.getMemberId());
+        assertThat(findMemberA.getMoney()).isEqualTo(8000);
+        assertThat(findMemberB.getMoney()).isEqualTo(12000);
+    }
+
+    @DisplayName("트랜잭션을 적용할 경우, 계좌 이체 중 예외가 발생하면 롤백된다")
+    @Test
+    void accountTransferException() throws SQLException {
+        // given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberEx = new Member(MEMBER_EX, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberEx);
+
+        // when
+        assertThatThrownBy(() ->
+                memberService.accountTransfer(memberA.getMemberId(), memberEx.getMemberId(), 2000))
+                .isInstanceOf(IllegalStateException.class);
+
+        // then
+        Member findMemberA = memberRepository.findById(memberA.getMemberId());
+        Member findMemberEx = memberRepository.findById(memberEx.getMemberId());
+
+        assertThat(findMemberA.getMoney()).isEqualTo(10000);
+        assertThat(findMemberEx.getMoney()).isEqualTo(10000);
+    }
+
+}
