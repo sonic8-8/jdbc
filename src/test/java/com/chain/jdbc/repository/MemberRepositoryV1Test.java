72991ee28bfc5fc9aaaaa8776b660666c1ae732c
이승환 <seaung13@gmail.com>
2025-01-13 18:26:59 +0900

    test: DriverManagerDataSource와 HikariDataSource의 CRUD 수행 시 커넥션 연결 및 사용 방식 비교 테스트 추가

---
+++ src/test/java/com/chain/jdbc/repository/MemberRepositoryV1Test.java
@@ -0,0 +1,64 @@
+package com.chain.jdbc.repository;
+
+import com.chain.jdbc.connection.ConnectionConst;
+import com.chain.jdbc.domain.Member;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+import static com.chain.jdbc.connection.ConnectionConst.*;
+import static com.chain.jdbc.connection.ConnectionConst.URL;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+class MemberRepositoryV1Test {
+
+    MemberRepositoryV1 repository;
+
+    @BeforeEach
+    void beforeEach() {
+
+        //DriverManagerDataSource
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+
+        //HikariDataSource
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+
+        repository = new MemberRepositoryV1(dataSource);
+    }
+
+    @DisplayName("DataSource로 커넥션 획득 방법을 추상화한 Repository로 CRUD 작업을 수행할 수 있다")
+    @Test
+    void crud() throws SQLException {
+        //save
+        Member member = new Member("memberV1", 10000);
+        repository.save(member);
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        assertThat(findMember).isEqualTo(member);
+
+        //update
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        //delete
+        repository.delete(updatedMember.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
+    }
+
+}
