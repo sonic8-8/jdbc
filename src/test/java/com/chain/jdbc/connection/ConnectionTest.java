eeffcb04e6ad5b8843b9ede5ec5aaa4429ecfe54
이승환 <seaung13@gmail.com>
2025-01-13 18:00:37 +0900

    test: 커넥션 획득 방법 추상화 - HikariDataSource를 사용한 커넥션 획득 방법 테스트(커넥션 풀)

--- src/test/java/com/chain/jdbc/connection/ConnectionTest.java
+++ src/test/java/com/chain/jdbc/connection/ConnectionTest.java
@@ -1,10 +1,13 @@
 package com.chain.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -12,6 +15,7 @@ import java.sql.SQLException;
 import static com.chain.jdbc.connection.ConnectionConst.*;
 
 @Slf4j
+@SpringBootTest
 public class ConnectionTest {
 
     @DisplayName("DriverManager를 사용해 커넥션을 획득할 수 있다")
@@ -24,13 +28,32 @@ public class ConnectionTest {
         log.info("connection={}, class={}", conn2, conn2.getClass());
     }
 
-    @DisplayName("DriverManagerDataSource를 사용해 커넥션을 획득할 수 있다")
+    @DisplayName("DriverManagerDataSource를 사용해 커넥션을 획득할 수 있다 - DataSource 구현체")
     @Test
     void driverManagerDataSource() throws SQLException {
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+
+        useDataSource(dataSource);
+    }
+
+    @DisplayName("HikariDataSource을 사용해 커넥션 풀에 커넥션을 생성할 수 있다 - DataSource 구현체")
+    @Test
+    void hikariDataSource() throws SQLException, InterruptedException {
+
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000);
+    }
+
+    private static void useDataSource(DataSource dataSource) throws SQLException {
         Connection conn1 = dataSource.getConnection();
         Connection conn2 = dataSource.getConnection();
-
         log.info("connection={}, class={}", conn1, conn1.getClass());
         log.info("connection={}, class={}", conn2, conn2.getClass());
     }
