e290d8832d7aca5dbdd537ddbec4253a610e68cc
이승환 <seaung13@gmail.com>
2025-01-15 17:19:17 +0900

    feat: MemberRepository, MemberRepositoryEx 인터페이스 추가

---
+++ src/main/java/com/chain/jdbc/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.chain.jdbc.repository;
+
+import com.chain.jdbc.domain.Member;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Member findById(String memberId);
+    void update(String memberId, int money);
+    void delete(String memberId);
+}
